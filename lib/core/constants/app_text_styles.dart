1cea9b0322d21acb176ff40ad7a17007f90550ff
philuv9-lang <philuv9@gmail.com>
2026-02-23 10:08:44 -0500

    Add typography styles using Roboto and SF Pro fonts

---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle heading1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  
+  static const TextStyle heading2 = TextStyle(
+    fontFamily: 'SF Pro',
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  
+  static const TextStyle body = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+  
+  static const TextStyle caption = TextStyle(
+    fontFamily: 'SF Pro',
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: Colors.grey,
+  );
+}
